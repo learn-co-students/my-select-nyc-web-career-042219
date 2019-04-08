2fa62e56d2b4cbe8710cbe8111bd7de5c2a91f4e
Carly Pecora <carly@carlypecora.com>
2019-04-08 17:50:36 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
+	new_col = []
+	i = 0
+	while i < collection.length
+		if yield(collection[i]) == true
+				new_col.push(collection[i])
+		end
+		i+= 1
  # your code here!
+	end
+ new_col
 end
